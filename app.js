d28ea430c38c622ad30544c53fb266cd3f24d907
Misha <mishaasl1996@gmail.com>
2026-09-13 23:33:33 +0200

    Shop-Bild für Level 5 auf newlvl5.jpg umgestellt

--- app.js
+++ app.js
@@ -493,7 +493,8 @@
         yellow:'sprites/newlvl1.jpg',
         red:'sprites/newlvl2.jpg',
         white:'sprites/newlvl3.jpg',
-        green:'sprites/newlvl4.jpg'
+        green:'sprites/newlvl4.jpg',
+        black:'sprites/newlvl5.jpg'
       };
       const img = shopImageOverrides[def.key] || ((typeof SKIN_IMAGES !== 'undefined' && SKIN_IMAGES[def.key]) ? SKIN_IMAGES[def.key] : '');
 
